be5c54103ebf7ac437fac8f5f69147bba58f6cec
HotBloodMan <18303011272@163.com>
2017-05-22 11:38:30 +0800

    ToolBar的基本使用

---
+++ app/src/main/java/com/example/a1/toolbar170522/ZhiHuActivity.java
@@ -0,0 +1,13 @@
+package com.example.a1.toolbar170522;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ZhiHuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhi_hu);
+    }
+}
